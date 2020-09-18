9e8344a40e434dd84809816864ca5c50aeb3b33c
aleucilima <aleuci.lima@gmail.com>
2020-09-17 21:26:51 -0300

    remove autentication

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -14,12 +14,6 @@ module.exports = {
         const { company, techs, price } = req.body
         const { user_id } = req.headers
 
-        const user = await User.findById(user_id)
-
-        if(!user) {
-            return res.status(400).json({ error: 'User does not exists' })
-        }
-
         const spot = await Spot.create({
             user: user_id,
             thumbnail: filename, 
