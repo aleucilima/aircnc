bc7aaa01b13326e16e914411ab9544cf5b6b2889
aleucilima <aleuci.lima@gmail.com>
2020-09-29 18:55:11 -0300

    Backend AirCnC

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const routes = require('./routes')
 
@@ -9,6 +10,8 @@ mongoose.connect('mongodb+srv://adminAcademy:root@academy.3mmz5.mongodb.net/acad
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+
+app.use(cors())
 
 app.use(express.json())
 
